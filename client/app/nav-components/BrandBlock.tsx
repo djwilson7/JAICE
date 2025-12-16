5861f79c5f6f2f2a863edfcb2c531669f729637a
djwilson7 <dontai.j.wilson@gmail.com>
2025-12-16 09:07:23 -0600

    Removed the search bar and updated the spacing of elements.

--- client/app/nav-components/BrandBlock.tsx
+++ client/app/nav-components/BrandBlock.tsx
@@ -4,16 +4,16 @@ export function BrandBlock() {
   const brandImg = useBrandImage();
 
   return (
-    <div className="brand-block items-center justify-end lg:justify-center gap-2">
+    <div className="brand-block items-center justify-end lg:justify-center">
       <div className="brandIcon">
         <img
           src={brandImg}
           alt="JAICE"
-          className="object-cover w-16 md:w-20 lg:w-24 animate-element"
+          className="object-cover w-20 md:w-22 lg:w-24 animate-element"
         />
       </div>
       <div
-        className="brand-tag text-lg md:text-xl lg:text-2xl animate-element"
+        className="brand-tag text-md md:text-xl lg:text-2xl whitespace-nowrap animate-element"
       >
         Simplify Your Job Hunt
       </div>
