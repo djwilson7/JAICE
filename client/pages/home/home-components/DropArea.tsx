95202e6daee1208c1afd6c1f969795e60cae413c
djwilson7 <dontai.j.wilson@gmail.com>
2025-12-17 18:12:02 -0600

    Updated the drop locations for increased contrast and animation smoothness.

--- client/pages/home/home-components/DropArea.tsx
+++ client/pages/home/home-components/DropArea.tsx
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import archiveIcon from "../../../assets/icons/folder.svg";
 import trashIcon from "../../../assets/icons/trash.svg";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 interface DropAreaProps {
   onDragEnter: (id: string) => void;
@@ -10,48 +11,83 @@ interface DropAreaProps {
 export function DropArea({ onDragEnter, onDragLeave }: DropAreaProps) {
   const dropAreaClass = "flex w-1/2 p-4 justify-center items-center gap-2";
 
+  const [hoveredArea, setHoveredArea] = useState<string | null>(null);
+
+  const archiveIconColor = hoveredArea === "archive" ? "orangeIcon" : "icon";
+  const trashIconColor = hoveredArea === "delete" ? "redIcon" : "icon";
+
   const handlePointerEnter = useCallback(
     (id: string) => {
+      setHoveredArea(id);
       onDragEnter(id);
     },
     [onDragEnter]
   );
 
   const handlePointerLeave = useCallback(() => {
+    setHoveredArea(null);
     onDragLeave();
   }, [onDragLeave]);
 
   return (
     <div className="drop-area">
-      <div className="flex flex-col gap-4 w-full h-full items-center justify-center">
-        <h3 className="primary-text">Drag to Archive or Delete</h3>
-
+      <div className="flex flex-col w-full h-full items-center justify-center">
+        {/* <h3 className="primary-text py-2">Drag to Archive or Delete</h3> */}
+        {/* <hr className="flex w-full border-dashed border-2 border-gray-400"/> */}
         <div className="flex w-full items-center justify-center">
-          <div
-            className={dropAreaClass}
+          <motion.div
+            className={`${dropAreaClass}`}
+            style={{
+              boxShadow: "inset 0px -15px 14px 0px rgba(255, 140, 0, 0.7)",
+            }}
             onPointerEnter={() => handlePointerEnter("archive")}
             onPointerLeave={handlePointerLeave}
+            animate={{
+              background:
+                hoveredArea === "archive"
+                  ? "rgba(255, 140, 0, 0.1)"
+                  : "transparent",
+            }}
           >
-            <img
-              src={archiveIcon}
-              alt="Archive Icon"
-              className="h-10 w-10 mr-2 icon"
-            />
-            <h2 className="primary-text">Archive</h2>
-          </div>
+            <motion.div
+              className="flex items-center justify-center"
+              animate={{ x: hoveredArea === "archive" ? -100 : 0 }}
+            >
+              <img
+                src={archiveIcon}
+                alt="Archive Icon"
+                className={`h-8 w-8 mr-2 ${archiveIconColor}`}
+              />
+              <h2 className="primary-text">Archive</h2>
+            </motion.div>
+          </motion.div>
 
-          <div
-            className={dropAreaClass}
+          <motion.div
+            className={`${dropAreaClass}`}
+            style={{
+              boxShadow: "inset 0px -15px 14px 0px rgba(255, 0, 0, 0.7)",
+            }}
             onPointerEnter={() => handlePointerEnter("delete")}
             onPointerLeave={handlePointerLeave}
+            animate={{
+              background:
+                hoveredArea === "delete"
+                  ? "rgba(255, 0, 0, 0.1)"
+                  : "transparent",
+            }}
           >
-            <img
-              src={trashIcon}
-              alt="Trash Icon"
-              className="h-10 w-10 mr-2 icon"
-            />
-            <h2 className="primary-text">Delete</h2>
-          </div>
+            <motion.div
+              className="flex items-center justify-center"
+              animate={{ x: hoveredArea === "delete" ? 100 : 0 }}
+            >
+              <img
+                src={trashIcon}
+                alt="Trash Icon"
+                className={`h-8 w-8 mr-2 ${trashIconColor}`}
+              />
+              <h2 className="primary-text">Trash</h2>
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </div>
