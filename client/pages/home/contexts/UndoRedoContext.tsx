74e65117da7324dbccc5691accd57d3c46b81a39
djwilson7 <dontai.j.wilson@gmail.com>
2025-12-19 10:37:43 -0600

    updated undo redo context

--- client/pages/home/contexts/UndoRedoContext.tsx
+++ client/pages/home/contexts/UndoRedoContext.tsx
@@ -1,31 +1,29 @@
 // UndoRedoContext.ts
 import { createContext } from "react";
-import type { UndoAction } from "@/types/undoAction";
+import type { SnapShotAction } from "@/types/undoAction";
 
 export interface UndoRedoContextType {
-  undoStack: UndoAction[];
-  redoStack: UndoAction[];
-  pushUndo: (action: UndoAction) => void;
-  performUndo: () => void;
-  performRedo: () => void;
-  isUndoRedoAvailable: boolean;
+  pushUndo: (action: SnapShotAction) => void;
+  undo: () => SnapShotAction | undefined;
+  redo: () => SnapShotAction | undefined;
   hasUndo: boolean;
   hasRedo: boolean;
 }
 
 export const UndoRedoContext = createContext<UndoRedoContextType>({
-  undoStack: [],
-  redoStack: [],
   pushUndo: () => {
     console.warn("pushUndo called outside UndoRedoProvider");
   },
-  performUndo: () => {
-    console.warn("performUndo called outside UndoRedoProvider");
+
+  undo: () => {
+    console.warn("undo called outside UndoRedoProvider");
+    return undefined;
   },
-  performRedo: () => {
-    console.warn("performRedo called outside UndoRedoProvider");
+  redo: () => {
+    console.warn("redo called outside UndoRedoProvider");
+    return undefined;
   },
-  isUndoRedoAvailable: false,
+
   hasUndo: false,
   hasRedo: false,
 });
