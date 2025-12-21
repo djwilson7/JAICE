62cf77db0930397f87fdd9c7bf01a35ccde47340
djwilson7 <dontai.j.wilson@gmail.com>
2025-12-21 11:11:17 -0600

    Added a drag start var to the drag context

--- client/pages/home/contexts/DragContext.tsx
+++ client/pages/home/contexts/DragContext.tsx
@@ -5,10 +5,12 @@ export interface DragContextType {
   isDragging: boolean;
   draggedId: string | null;
   dragTarget: DragTarget;
+  dragStart: string | null;
 
   setIsDragging: (value: boolean) => void;
   setDraggedId: (id: string | null) => void;
   setDragTarget: (target: DragTarget) => void;
+  setDragStart: (start: string | null) => void;
 }
 
 export const DragContext = createContext<DragContextType | null>(null);
