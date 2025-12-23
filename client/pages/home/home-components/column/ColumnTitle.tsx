b9be955bb32fc10753d40529ef8918e046e8708d
djwilson7 <dontai.j.wilson@gmail.com>
2025-12-23 16:21:55 -0600

    fixed deps

--- client/pages/home/home-components/column/ColumnTitle.tsx
+++ client/pages/home/home-components/column/ColumnTitle.tsx
@@ -1,11 +1,5 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
-
 interface ColumnTitleProps {
   title: string;
-  index: number;
-  onToggle?: (index: number) => void;
-  canToggle?: boolean;
 }
 
 const columnDescriptions = {
@@ -18,58 +12,16 @@ const columnDescriptions = {
   Review: "These applications need your review.",
 };
 
-export function ColumnTitle({
-  title,
-  index,
-  onToggle,
-  canToggle,
-}: ColumnTitleProps) {
-  const [isSelected, setIsSelected] = useState(title);
-
+export function ColumnTitle({ title }: ColumnTitleProps) {
   const headerClass = "flex w-full items-center justify-center";
-
-  if (!canToggle) {
-  return (
-      <div className="flex flex-col w-full items-center justify-center">
-      <div className={headerClass}>
-          <div className="flex w-full items-center justify-center">
-            <h2>{title}</h2>
-          </div>
-        </div>
-        <div className="flex w-full items-center justify-center">
-          <small className="text-center secondary-text">
-            {columnDescriptions[title as keyof typeof columnDescriptions]}
-          </small>
-        </div>
-      </div>
-    );
-  }
-  const toggleOptions = ["Accepted", "Rejected"];
-
-  const handleToggle = () => {
-    if (onToggle) {
-      onToggle(index);
-      const currentIndex = toggleOptions.indexOf(isSelected);
-      const nextIndex = (currentIndex + 1) % toggleOptions.length;
-      setIsSelected(toggleOptions[nextIndex]);
-    }
-  };
 
   return (
     <div className="flex flex-col w-full items-center justify-center">
-      <motion.div
-        className={headerClass}
-        onClick={handleToggle}
-        style={{ cursor: "pointer" }}
-        title={"Cycle between Accepted and Rejected. Click to toggle."}
-      >
-        <motion.h2 animate={{ scale: title === toggleOptions[0] ? 1 : 0.6 }}>
-          {toggleOptions[0]}
-        </motion.h2>
-        <motion.h2 animate={{ scale: title === toggleOptions[1] ? 1 : 0.6 }}>
-          {toggleOptions[1]}
-        </motion.h2>
-      </motion.div>
+      <div className={headerClass}>
+        <div className="flex w-full items-center justify-center">
+          <h2>{title}</h2>
+        </div>
+      </div>
       <div className="flex w-full items-center justify-center">
         <small className="text-center secondary-text">
           {columnDescriptions[title as keyof typeof columnDescriptions]}
@@ -77,38 +29,4 @@ export function ColumnTitle({
       </div>
     </div>
   );
-
-  // const toggleOptions = ["Accepted", "Rejected"];
-
-  // const handleToggle = () => {
-  //   if (onToggle) {
-  //     onToggle(index);
-  //     const currentIndex = toggleOptions.indexOf(isSelected);
-  //     const nextIndex = (currentIndex + 1) % toggleOptions.length;
-  //     setIsSelected(toggleOptions[nextIndex]);
-  //   }
-  // };
-
-  // return (
-  //   <div className="flex flex-col w-full items-center justify-center overflow-hidden">
-  //     <motion.div
-  //       className={headerClass}
-  //       onClick={handleToggle}
-  //       style={{ cursor: "pointer" }}
-  //       title={"Cycle between Accepted and Rejected. Click to toggle."}
-  //     >
-  //       <motion.h2 animate={{ scale: title === toggleOptions[0] ? 1 : 0.6 }} className="whitespace-nowrap text-ellipsis">
-  //         {toggleOptions[0]}
-  //       </motion.h2>
-  //       <motion.h2 animate={{ scale: title === toggleOptions[1] ? 1 : 0.6 }} className="whitespace-nowrap text-ellipsis">
-  //         {toggleOptions[1]}
-  //       </motion.h2>
-  //     </motion.div>
-  //     <div className="flex w-full items-center justify-center overflow-hidden whitespace-nowrap">
-  //       <small className="text-center secondary-text whitespace-nowrap text-ellipsis">
-  //         {columnDescriptions[title as keyof typeof columnDescriptions]}
-  //       </small>
-  //     </div>
-  //   </div>
-  // );
 }
